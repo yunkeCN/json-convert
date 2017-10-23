d60adca25b61e1953db45f52b9a57f27fa30ad63
熊杰 <xiongj01@mysoft.com.cn>
2017-10-23 11:01:49 +0800

    示例注释掉不要每次都占用cpu

--- test/test.ts
+++ test/test.ts
@@ -1,29 +1,29 @@
-import { suite, test } from "mocha-typescript"
-import { Unit } from "../src/index";
-import { assert } from "chai";
+// import { suite, test } from "mocha-typescript"
+// import { Unit } from "../src/index";
+// import { assert } from "chai";
 
-describe("vanilla bdd", () => {
-    it("test", () => {
-    });
-});
+// describe("vanilla bdd", () => {
+//     it("test", () => {
+//     });
+// });
 
-suite("vanilla tdd", () => {
-    test("test", () => {
-    });
-});
+// suite("vanilla tdd", () => {
+//     test("test", () => {
+//     });
+// });
 
-@suite class UnitTest extends Unit {
-    @test "big is true with big number"() {
-        assert(this.big(200));
-    }
-    @test "big is false with small number"() {
-        assert(!this.big(50));
-    }
-}
+// @suite class UnitTest extends Unit {
+//     @test "big is true with big number"() {
+//         assert(this.big(200));
+//     }
+//     @test "big is false with small number"() {
+//         assert(!this.big(50));
+//     }
+// }
 
-suite("nested class suite", () => {
-    @suite class NestedTest {
-        @test "a test"() {
-        }
-    }
-});
+// suite("nested class suite", () => {
+//     @suite class NestedTest {
+//         @test "a test"() {
+//         }
+//     }
+// });
